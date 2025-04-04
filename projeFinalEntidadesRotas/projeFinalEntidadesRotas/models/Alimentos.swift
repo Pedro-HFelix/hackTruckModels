d767b7983fe9cf65510c01366c00693782e270b8
PEDRO HENRIQUE FÉLIX DOS SANTOS <108374688+Pedro-HFelix@users.noreply.github.com>
2025-04-04 17:31:22 +0000

    feat: criação de struct comidas

--- projeFinalEntidadesRotas/projeFinalEntidadesRotas/models/Alimentos.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Alimentos.swift
-//  projeFinalEntidadesRotas
-//
-//  Created by Turma01-22 on 04/04/25.
-//
-
-import Foundation
---
+++ projeFinalEntidadesRotas/projeFinalEntidadesRotas/models/Food.swift
@@ -0,0 +1,25 @@
+//
+//  Alimentos.swift
+//  projeFinalEntidadesRotas
+//
+//  Created by Turma01-22 on 04/04/25.
+//
+
+import Foundation
+
+struct Food: Identifiable, Codable {
+    var id: Int?
+    var name: String?
+    var description: String?
+    var period: Period?
+}
+
+enum Period: String, CaseIterable, Codable {
+    case manha = "Manhã"
+    case tarde = "Tarde"
+    case noite = "Noite"
+    
+    var localizedDescription: String {
+        return self.rawValue
+    }
+}
